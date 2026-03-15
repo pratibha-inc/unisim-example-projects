39cc38109569580fd593569cc97ce6ff4453a0ea
tumul-unisim <mailtotumulkumarsinha@gmail.com>
2026-03-15 15:03:49 +0530

    fix: pico_button - sync Pico ID in config and fix main.py script

---
+++ pico_button/src/pico-cnpn9x/main.py
@@ -0,0 +1,13 @@
+from machine import Pin
+import time
+
+# Use PULL_UP for GND-wired buttons
+btn = Pin(0, Pin.IN, Pin.PULL_UP)
+led = Pin(1, Pin.OUT)
+
+while True:
+    if not btn.value():  # 0 = Pressed
+        led.on()
+    else:                # 1 = Released
+        led.off()
+    time.sleep(0.05)
